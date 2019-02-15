853e8bf272cfa205165363c208530f28b66ca9a5
GeorgeTailor <yuriy.kravets@zooplus.com>
2019-02-15 15:12:40 +0100

    Initial version of searchable select

--- zoo-modules/select-module/SearchableSelect.svelte
+++ zoo-modules/select-module/SearchableSelect.svelte
@@ -1,8 +1,10 @@
+<svelte:options tag="zoo-log-searchable-select"></svelte:options>
 <div class="searchable-select-box">
-	<input class="searchable-input" 
-		placeholder="{searchableplaceholder}" 
-		type="text" on:keyup="{event => handleSearchChange(event)}" 
-		bind:this={searchableInput}>
+	<zoo-log-input infotext="Start typing to limit select options"
+		placeholder="{searchableplaceholder}"
+		type="text" on:keyup="{event => handleSearchChange(event)}">
+		<input slot="inputelement" type="text" placeholder="input" bind:this={searchableInput}/>
+	</zoo-log-input>
 	<zoo-log-select labeltext="{labeltext}" 
 		searchable="{true}"
 		labelposition="{labelposition}"
@@ -23,33 +25,38 @@
 <style>
 </style>
 <script>
-	export default {
-	  tag: "zoo-log-searchable-select",
-	  data() {
-	    return {
-			labelposition: "top",
-	        labeltext: "",
-	        linktext: "",
-	        linkhref: "",
-	        linktarget: "about:blank",
-	        inputerrormsg: "",
-	        infotext: "",
-	        valid: true,
-			_slottedSelect: undefined,
-			options: [
-				{
-					text: 'text',
-					value: 'value'
-				}
-			]
-	    };
-	  },
-	  methods: {
-		  handleSearchChange(event) {
-			console.log(event);
-			const inputVal = this.refs.searchableInput.value;
-			console.log(inputVal);
+	import { onMount } from 'svelte';
+
+	export let labelposition = "top";
+	export let labeltext = "";
+	export let linktext = "";
+	export let linkhref = "";
+	export let linktarget = "about:blank";
+	export let inputerrormsg = "";
+	export let infotext = "";
+	export let valid = true;
+	export let searchableplaceholder = '';
+	let searchableInput;
+	let _slottedSelect;
+	let _optionsBackup;
+	let options = [
+		{
+			text: 'text',
+			value: 'value'
+		},
+		{
+			text: 'random',
+			value: 'random'
 		}
-	  }
-	}
+	];
+
+	onMount(() => {
+		_optionsBackup = options.slice();
+	});
+
+	const handleSearchChange = event => {
+		const inputVal = searchableInput.value;
+		console.log(inputVal);
+		options = _optionsBackup.filter(option => option.text.indexOf(inputVal) != -1);
+	};
 </script>

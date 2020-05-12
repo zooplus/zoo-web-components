924fb0191d3310d50b9c4ea2c5bd4e35a92c4a7a
GeorgeTailor <yucikkravets@gmail.com>
2020-05-12 17:37:57 +0200

    Fix stylelint

--- zoo-modules/grid-module/GridPaginator.svelte
+++ zoo-modules/grid-module/GridPaginator.svelte
@@ -15,6 +15,7 @@
 		<template id="arrow">
 			<style>
 				.btn.next svg {transform: rotate(-90deg);}
+
 				.btn.prev svg {transform: rotate(90deg);}
 			</style>
 			<svg class="arrow" width="24" height="24" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/></svg>

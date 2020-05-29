0a802177af7c6652b341584142d5fd3581189279
GeorgeTailor <yucikkravets@gmail.com>
2020-05-29 20:03:26 +0200

    update dependencies and remove tests that dont make sense

--- zoo-modules/feedback-module/feeback.spec.js
+++ zoo-modules/feedback-module/feeback.spec.js
@@ -1,73 +1,16 @@
 describe('Zoo feedback', function() {
 	describe('Feedback', () => {
 		it('should create default feedback', async() => {
-			const feedbackAttrs = await page.evaluate(() => {
+			const retText = await page.evaluate(() => {
 				let feedback = document.createElement('zoo-feedback');
+				const span = document.createElement('span');
+				span.innerHTML = 'example';
+				feedback.appendChild(span);
 				document.body.appendChild(feedback);
-				const infoIcon = feedback.shadowRoot.querySelector('.info-rounded-circle-o');
-				const text = feedback.shadowRoot.querySelector('span').innerHTML;
-				const feedbackAttrs = {
-					infoIcon: infoIcon,
-					text: text
-				};
-				return feedbackAttrs;
+				const text = feedback.shadowRoot.querySelector('slot').assignedNodes()[0].innerHTML;
+				return text;
 			});
-			expect(feedbackAttrs.infoIcon).to.be.not.undefined;
-			expect(feedbackAttrs.text).equal('');
-		});
-
-		it('should create feedback info with text', async() => {
-			const feedbackAttrs = await page.evaluate(() => {
-				let feedback = document.createElement('zoo-feedback');
-				feedback.type = 'info';
-				feedback.text = 'test-text';
-				document.body.appendChild(feedback);
-				const infoIcon = feedback.shadowRoot.querySelector('.info-rounded-circle-o');
-				const text = feedback.shadowRoot.querySelector('span').innerHTML;
-				const feedbackAttrs = {
-					infoIcon: infoIcon,
-					text: text
-				};
-				return feedbackAttrs;
-			});
-			expect(feedbackAttrs.infoIcon).to.be.not.undefined;
-			expect(feedbackAttrs.text).equal('test-text');
-		});
-
-		it('should create feedback success with text', async() => {
-			const feedbackAttrs = await page.evaluate(() => {
-				let feedback = document.createElement('zoo-feedback');
-				feedback.type = 'success';
-				feedback.text = 'test-text';
-				document.body.appendChild(feedback);
-				const infoIcon = feedback.shadowRoot.querySelector('.happiness');
-				const text = feedback.shadowRoot.querySelector('span').innerHTML;
-				const feedbackAttrs = {
-					infoIcon: infoIcon,
-					text: text
-				};
-				return feedbackAttrs;
-			});
-			expect(feedbackAttrs.infoIcon).to.be.not.undefined;
-			expect(feedbackAttrs.text).equal('test-text');
-		});
-
-		it('should create feedback error with text', async() => {
-			const feedbackAttrs = await page.evaluate(() => {
-				let feedback = document.createElement('zoo-feedback');
-				feedback.type = 'error';
-				feedback.text = 'test-text';
-				document.body.appendChild(feedback);
-				const infoIcon = feedback.shadowRoot.querySelector('.error');
-				const text = feedback.shadowRoot.querySelector('span').innerHTML;
-				const feedbackAttrs = {
-					infoIcon: infoIcon,
-					text: text
-				};
-				return feedbackAttrs;
-			});
-			expect(feedbackAttrs.infoIcon).to.be.not.undefined;
-			expect(feedbackAttrs.text).equal('test-text');
+			expect(retText).equal('example');
 		});
 	});
 });

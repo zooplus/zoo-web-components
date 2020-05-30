2cf8ee5363864ba03439e85c45f47331c3935664
GeorgeTailor <yucikkravets@gmail.com>
2020-05-30 15:15:34 +0200

    Migrated zoo-collapsable-list to slots
    added documentation for zoo-collapsable-list

--- zoo-modules/collapsable-list-module/CollapsableList.svelte
+++ zoo-modules/collapsable-list-module/CollapsableList.svelte
@@ -1,95 +1,32 @@
 <svelte:options tag="zoo-collapsable-list"></svelte:options>
-<ul>
-	{#each items as item, idx}
-		<li class="item" class:active="{_items && _items[idx].active}"> 
-			<span class="header" on:click="{e => handleItemHeaderClick(e, idx)}">
-				{item.header}
-				<svg width="24" height="24" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/></svg>
-			</span>
-			<slot name="item{idx}"></slot>
-		</li>
-	{/each}
-</ul>
+<slot bind:this={itemSlot}></slot>
 
 <style type="text/scss">
-	@import "variables";
-
-	.item ::slotted(*) {
-		display: none;
-	}
-
-	.item.active ::slotted(*) {
-		display: initial;
-	}
-
-	ul {
-		padding: 0;
-	}
-
-	.item {
-		position: relative;
-		color: $grey-mid;
-		list-style-type: none;
-		padding: 0 10px;
-		border: 0;
-
-		&.active {
-			border: 1px solid rgba(0, 0, 0, 0.2);
-
-			.header {
-				color: var(--primary-dark, #{$primary-dark});
-
-				svg {
-					fill: var(--primary-dark, #{$primary-dark});
-					transform: rotateX(180deg);
-				}
-			}
-		}
-	}
-	
-	.header {
+	:host {
 		display: flex;
-		align-items: center;
-		height: 8px;
-		padding: 20px 0;
-		font-size: $p1-size;
-		line-height: $p1-line-height;
-		color: var(--primary-mid, #{$primary-mid});
-		font-weight: bold;
-		cursor: pointer;
-
-		svg {
-			display: flex;
-			margin-left: auto;
-			fill: var(--primary-mid, #{$primary-mid});
-			transition: transform 0.3s;
-		}
+		flex-direction: column;
 	}
 </style>
 
 <script>
-	import { beforeUpdate } from 'svelte';
-	export let items = [];
-	let _items;
+	import { onMount } from 'svelte';
+	let itemSlot;
+	let prevActiveItem;
 
-	beforeUpdate(() => {
-		if (_items != items) {
-			_items = items;
-		}
+	onMount(() => {
+		itemSlot.addEventListener("slotchange", () => {
+			const items = itemSlot.assignedNodes();
+			items[0].setAttribute('active', true);
+			prevActiveItem = items[0];
+
+			for (const item of items) {
+				item.addEventListener('click', () => {
+					if (item.hasAttribute('active')) return;
+					prevActiveItem.removeAttribute('active');
+					prevActiveItem = item;
+					item.setAttribute('active', true);
+				});
+			}
+		});
 	});
-
-	const handleItemHeaderClick = (e, id) => {
-		if (_items[id].active) {
-			_items[id].active = false;
-		} else {
-			clearActiveStatus();
-			_items[id].active = true;
-		}
-	}
-
-	const clearActiveStatus = () => {
-		for (const item of _items) {
-			item.active = false;
-		}
-	}
 </script>
--- zoo-modules/collapsable-list-module/zoo-collapsable-list.stories.js
+++ zoo-modules/collapsable-list-module/zoo-collapsable-list.stories.js
@@ -1,21 +1,33 @@
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, text } from '@storybook/addon-knobs';
 import { html } from 'lit-html';
+import mdx from './zoo-collapsable-list.mdx';
 
 export default {
-  title: 'Docs/Collapsable list',
-  component: 'zoo-collapsable-list',
-  decorators: [withKnobs]
+	title: 'Docs/Collapsable list',
+	component: 'zoo-collapsable-list',
+	decorators: [withKnobs],
+	parameters: {
+		docs: {
+			page: mdx,
+		},
+	}
 };
 
 export const zooCollapsableList = () => {
+	const dataGroup = 'Data';
+	let header1 = text('header1', 'Header 1', dataGroup);
+	let header2 = text('header2', 'Header 2', dataGroup);
+	let content1 = text('content1', 'Content 1', dataGroup);
+	let content2 = text('content2', 'Content 2', dataGroup);
+
 	return html`<zoo-collapsable-list>
-					<span slot="item0header">First header</span>
-					<zoo-collapsable-list-item slot="item0">
-						<span>First item</span>
+					<zoo-collapsable-list-item slot="item">
+						<span slot="header">${header1}</span>
+						<div slot="content">${content1}</div>
 					</zoo-collapsable-list-item>
-					<span slot="item1header">Second header</span>
-					<zoo-collapsable-list-item slot="item1">
-						<span>Second item</span>
+					<zoo-collapsable-list-item slot="item">
+						<span slot="header">${header2}</span>
+						<div slot="content">${content2}</div>
 					</zoo-collapsable-list-item>
 				</zoo-collapsable-list>`
 };

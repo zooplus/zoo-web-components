eedb416829985e21835f5a5e088f252332080d4c
GeorgeTailor <yucikkravets@gmail.com>
2020-12-02 10:09:35 +0100

    run puppeteer on github

--- zoo-modules/shared/test.setup.spec.js
+++ zoo-modules/shared/test.setup.spec.js
@@ -2,7 +2,13 @@ const puppeteer = require('puppeteer');
 const axe = require('axe-core');
 
 beforeAll(async () => {
-	global.browser = await puppeteer.launch({headless: true});
+	global.browser = await puppeteer.launch({
+		headless: true,
+		args: [
+			`--no-sandbox`
+			`--disable-setuid-sandbox`
+		],
+	});
 	global.page = await global.browser.newPage();
 	global.axe = axe;
 	await global.page.goto('http://localhost:5000');

8d2e0f6967f62dcb86adc31c1390449ec7823b9d
GeorgeTailor <yucikkravets@gmail.com>
2020-12-02 10:54:10 +0100

    set node env for unix systems

--- zoo-modules/helpers/test.setup.js
+++ zoo-modules/helpers/test.setup.js
@@ -1,11 +1,12 @@
 const puppeteer = require('puppeteer');
 const axe = require('axe-core');
+const jasmine = require('jasmine');
 
 beforeAll(async () => {
+	jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
 	global.browser = await puppeteer.launch({
 		headless: true,
-		args: ['--no-sandbox', '--disable-setuid-sandbox'],
-		slowMo: 50
+		args: ['--no-sandbox', '--disable-setuid-sandbox']
 	});
 	global.page = await global.browser.newPage();
 	global.axe = axe;

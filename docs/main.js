69f176f9c9cba83d1bc8e5158e111104c8827ecb
ZOOPLUS\zp2106 <dawid.wolanin@zooplus.com>
2021-03-29 15:58:01 +0200

    refactor

--- docs/main.js
+++ docs/main.js
@@ -147,7 +147,7 @@ let data = [
 	{ createdDate: today, status: 'READY', maxWeight: '10 kg', deliveryDate: today, noOfPieces: 5, price: '12 EUR' }
 ];
 
-const getRow = (d, i, template, idx, withEmptyActions) => {
+const getRow = (d, i, template, idx) => {
 	const clone = template.cloneNode(true);
 	const row = clone.children[0];
 	// valid
@@ -196,25 +196,21 @@ const getRow = (d, i, template, idx, withEmptyActions) => {
 	price.innerHTML = d.price;
 	row.appendChild(price);
 
-	if (withEmptyActions) {
-		// actions
-		const actions = document.createElement('div');
-		row.appendChild(actions);
-	}
-
 	return clone;
 };
 
 const grids = document.querySelectorAll('zoo-grid');
 let idx = 0;
 for (const grid of grids) {
-	data.forEach((d, i) => {
-		const simpleRow = document.querySelector('#simple-row').content;
-		const clone = getRow(d, i, simpleRow, idx * data.length, idx === 2);
-		grid.appendChild(clone);
-	});
-	if (idx == 0) grid.setAttribute('resizable', true);
-	idx += 1;
+	if (idx !== 2) {
+		data.forEach((d, i) => {
+			const simpleRow = document.querySelector('#simple-row').content;
+			const clone = getRow(d, i, simpleRow, idx * data.length);
+			grid.appendChild(clone);
+		});
+		if (idx == 0) grid.setAttribute('resizable', true);
+		idx += 1;
+	}
 }
 
 document.querySelector('form').addEventListener('submit', e => {
--- src/zoo-modules/grid-modules/grid-row-module/grid-row.js
+++ src/zoo-modules/grid-modules/grid-row-module/grid-row.js
@@ -4,22 +4,6 @@
 export class GridRow extends HTMLElement {
 	constructor() {
 		super();
-
-		this.shadowRoot.querySelector('slot[name="row-details"]')
-			.addEventListener('slotchange', () => {
-				this.setRowContentHeightCSSProperty();
-			});
-	}
-
-	setRowContentHeightCSSProperty() {
-		const rowContent = this.shadowRoot.querySelector('slot[name="row-content"]').assignedElements()[0];
-		if (rowContent) {
-			const defaultContentHeight = 'auto';
-			this.style.setProperty(
-				'--grid-row-content-height',
-				rowContent.getAttribute('data-height') || defaultContentHeight
-			);
-		}
 	}
 }
 

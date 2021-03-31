8cdf62cc569182ebd019fa39d3427e7db912f0a8
GeorgeTailor <yucikkravets@gmail.com>
2021-03-31 16:10:40 +0200

    Build both a package with every single component and a file per component

--- rollup.config.js
+++ rollup.config.js
@@ -14,25 +14,66 @@ const plugins = [
 	}),
 ];
 
+const createConfig = (filename) => ({
+	input: `src/zoo-modules/${filename}`,
+	output: {
+		file: dev ? `docs/components/${filename}` : `docs/components/${filename}`,
+		format: 'iife',
+		name: 'filename',
+	},
+	plugins
+});
+
 export default [
 	{
 		input: 'src/components.js',
-		output: {
-			sourcemap: true,
-			format: 'iife',
-			file: dev ? 'docs/components/components.js' : 'dist/zoo-web-components.js',
-			name: 'zooWebComponents'
-		},
+		output: [
+			{
+				sourcemap: true,
+				format: 'iife',
+				dir: dev ? 'docs/components' : 'dist',
+				name: 'zooWebComponents'
+			},
+			{
+				sourcemap: true,
+				format: 'esm',
+				dir: dev ? 'docs/components/esm' : 'dist/esm',
+				preserveModules: true
+			}
+		],
 		plugins: plugins
 	},
-	{
-		input: 'src/components.js',
-		output: {
-			sourcemap: true,
-			format: 'esm',
-			file: dev ? 'docs/components/components-esm.js' : 'dist/zoo-web-components-esm.js',
-			name: 'zooWebComponents'
-		},
-		plugins: plugins
-	}
+	...[
+		'form-modules/info-module/info.js',
+		'form-modules/label-module/label.js',
+		'form-modules/input-module/input.js',
+		'form-modules/checkbox-module/checkbox.js',
+		'form-modules/radio-module/radio.js',
+		'form-modules/select-module/select.js',
+		'form-modules/searchable-select-module/searchable-select.js',
+		'form-modules/quantity-control-module/quantity-control.js',
+		'form-modules/toggle-switch-module/toggle-switch.js',
+		'form-modules/button-module/button.js',
+		'grid-modules/grid-module/grid.js',
+		'grid-modules/grid-header-module/grid-header.js',
+		'grid-modules/grid-row-module/grid-row.js',
+		'misc-modules/header-module/header.js',
+		'misc-modules/modal-module/modal.js',
+		'misc-modules/footer-module/footer.js',
+		'misc-modules/feedback-module/feedback.js',
+		'misc-modules/tooltip-module/tooltip.js',
+		'misc-modules/link-module/link.js',
+		'misc-modules/navigation-module/navigation.js',
+		'misc-modules/toast-module/toast.js',
+		'misc-modules/collapsable-list-module/collapsable-list.js',
+		'misc-modules/collapsable-list-item-module/collapsable-list-item.js',
+		'misc-modules/spinner-module/spinner.js',
+		'misc-modules/paginator-module/paginator.js',
+		'misc-modules/preloader-module/preloader.js',
+		'misc-modules/attention-icon-module/attention-icon.js',
+		'misc-modules/cross-icon-module/cross-icon.js',
+		'misc-modules/arrow-icon-module/arrow-icon.js',
+		'misc-modules/paw-icon-module/paw-icon.js',
+		'misc-modules/tag-module/tag.js'
+	].map((filename) => createConfig(filename))
 ];

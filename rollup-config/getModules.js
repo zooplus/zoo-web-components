8a7700980850c8068a9ea680fef9f51c0e5282ba
GeorgeTailor <yucikkravets@gmail.com>
2021-04-02 16:17:00 +0200

    modularize things a little

---
+++ rollup-config/getModules.js
@@ -0,0 +1,37 @@
+import {plugins} from './plugins.js';
+import path from 'path';
+import fs from 'fs';
+
+let dev = process.env.NODE_ENV == 'local';
+
+const createConfig = (filePath, plugins) => {
+	const fileName = filePath.replace('./src/zoo-modules/', '');
+	const shortName = fileName.substring(fileName.lastIndexOf('/') + 1, fileName.lastIndexOf('.')).split('-').join('')
+	return {
+		input: filePath,
+		output: {
+			file: dev ? `docs/components/${fileName}` : `dist/${fileName}`,
+			format: 'iife',
+			name: shortName,
+		},
+		plugins
+	}
+};
+
+
+function getFiles(nextPath, modules) {
+	if(fs.existsSync(nextPath) && fs.lstatSync(nextPath).isDirectory()) {
+		const nextDirPath = fs.readdirSync(nextPath);
+		nextDirPath.forEach(filePath => getFiles(`${nextPath}/${filePath}`, modules));
+	} else {
+		if (nextPath.indexOf('-module/') > -1 && nextPath.indexOf('.js') > -1) {
+			modules.push(nextPath);
+		}
+	}
+}
+
+export function getModules() {
+	let modules = [];
+	getFiles('./src/zoo-modules', modules);
+	return modules.map(modulePath => createConfig(modulePath));
+}
--- rollup.config.js
+++ rollup.config.js
@@ -1,49 +1,9 @@
-import { terser } from 'rollup-plugin-terser';
-import injectInnerHTML from './injectInnerHTML.js';
-import { watcher, noOpWatcher } from './watcher.js';
-import path from 'path';
-import fs from 'fs';
+import { plugins } from './rollup-config/plugins.js';
+import { getModules } from './rollup-config/getModules.js';
 
 let dev = process.env.NODE_ENV == 'local';
 
-const plugins = [
-	injectInnerHTML(),
-	dev ? watcher() : noOpWatcher(),
-	dev ? noOpWatcher() : terser({
-		module: true,
-		keep_classnames: true
-	}),
-];
-
-const createConfig = (filePath) => {
-	const fileName = filePath.replace('./src/zoo-modules/', '');
-	const shortName = fileName.substring(fileName.lastIndexOf('/') + 1, fileName.lastIndexOf('.')).split('-').join('')
-	return {
-		input: filePath,
-		output: {
-			file: dev ? `docs/components/${fileName}` : `dist/${fileName}`,
-			format: 'iife',
-			name: shortName,
-		},
-		plugins
-	}
-};
-
-let modules = [];
-function getFiles(nextPath) {
-	if(fs.existsSync(nextPath) && fs.lstatSync(nextPath).isDirectory()) {
-		const nextDirPath = fs.readdirSync(nextPath);
-		nextDirPath.forEach(filePath => getFiles(`${nextPath}/${filePath}`));
-	} else {
-		if (nextPath.indexOf('-module/') > -1 && nextPath.indexOf('.js') > -1) {
-			modules.push(nextPath);
-		}
-	}
-}
-
-!dev && getFiles('./src/zoo-modules');
-modules = modules.map(modulePath => createConfig(modulePath));
-
+const modules = !dev ? getModules() : [];
 export default [
 	{
 		input: 'src/zoo-web-components.js',

232894caec0ddbd4df13a7be1e090e40587b570e
GeorgeTailor <GeorgeTailor@users.noreply.github.com>
2021-07-01 09:30:38 +0000

    deploy: 322326e2735fdba92bc0a35c7b166ee01144f5d0

--- components/esm/zoo-modules/form/input-tag/input-tag.js
+++ components/esm/zoo-modules/form/input-tag/input-tag.js
@@ -1,9 +1,9 @@
 import { registerComponents } from '../../common/register-components.js';
-import { Input } from '../input/input.js';
-import { Select } from '../select/select.js';
 import { InputTagOption } from './input-tag-option.js';
 import { FormElement } from '../common/FormElement.js';
 import { CrossIcon } from '../../icon/cross-icon/cross-icon.js';
+import { InfoMessage } from '../info/info.js';
+import { Label } from '../label/label.js';
 
 /**
  * @injectHTML
@@ -11,7 +11,7 @@ import { CrossIcon } from '../../icon/cross-icon/cross-icon.js';
 class InputTag extends FormElement {
 	constructor() {
 		super();this.attachShadow({mode:'open'}).innerHTML=`<style>#input-wrapper,#tag-options{gap:5px;box-sizing:border-box}#input-wrapper,zoo-info{grid-column:span 2}:host{display:grid;grid-gap:3px;width:100%;height:max-content;box-sizing:border-box}#input-wrapper{display:flex;flex-wrap:wrap;align-items:center;height:max-content;font-size:14px;line-height:20px;padding:13px 15px;border:1px solid #767676;border-radius:5px;color:#555;position:relative;overflow:visible;z-index:2}:host(:focus-within) #input-wrapper{border:2px solid #555;padding:12px 14px}:host([invalid]) #input-wrapper{border:2px solid var(--warning-mid);padding:12px 14px}::slotted(input){border:0;min-width:50px;flex:1 0 auto;outline:0;font-size:14px;line-height:20px;color:#555}zoo-label{grid-row:1}#tag-options{display:none;position:absolute;flex-wrap:wrap;background:#fff;padding:5px;border:1px solid #555;border-radius:0 0 3px 3px;left:-1px;top:90%;border-top:0;width:calc(100% + 2px)}:host(:focus-within) #tag-options,:host([invalid]) #tag-options{border-width:2px;width:calc(100% + 4px);left:-2px}:host([invalid]) #tag-options{border-color:var(--warning-mid)}:host([show-tags]) #tag-options{display:flex}::slotted([slot=select]){display:none}zoo-cross-icon{cursor:pointer;--icon-color:var(--primary-mid)}::slotted(zoo-input-tag-option){flex:1 0 30%}</style><zoo-label><slot name="label"></slot></zoo-label><div id="input-wrapper"><slot name="input"></slot><div id="tag-options"><slot name="tag-option"></slot><slot name="no-results"></slot></div></div><zoo-info role="status"><slot name="info"></slot></zoo-info><zoo-info role="alert"><slot name="error"></slot></zoo-info><slot name="select"></slot>`;
-		registerComponents(Input, Select, InputTagOption, CrossIcon);
+		registerComponents(Label, InfoMessage, InputTagOption, CrossIcon);
 		this.inputSlot = this.shadowRoot.querySelector('slot[name="input"]');
 		this.inputSlot.addEventListener('slotchange', e => {
 			const input = [...e.target.assignedElements()].find(el => el.tagName === 'INPUT');

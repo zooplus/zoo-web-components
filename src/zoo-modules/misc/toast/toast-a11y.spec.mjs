09535ec0317d905384c7b02727a390fb0d48d794
Mariusz <61865578+mpad0x55@users.noreply.github.com>
2026-01-29 17:33:30 +0100

    test(a11y): enhance accessibility tests for zoo-toast component (#66)
    * test(a11y): enhance accessibility tests for zoo-toast component

--- src/zoo-modules/misc/toast/toast-a11y.spec.mjs
+++ src/zoo-modules/misc/toast/toast-a11y.spec.mjs
@@ -1,11 +1,11 @@
 describe('Zoo toast', function() {
 	it('should pass accessibility tests', async() => {
-		const results = await page.evaluate(async() => {
+		const results = await page.evaluate(async() => {		
 			document.body.innerHTML = `<zoo-toast closelabel="close popup">
 				<span slot="content">Search for more than 8.000 products.</span>
 			</zoo-toast>`;
 			document.querySelector('zoo-toast').show();
-			// wait for animation to finish
+			// wait for animation and styles to finish
 			await new Promise(res => setTimeout(() => res(), 330));
 			return await axe.run('zoo-toast');
 		});
